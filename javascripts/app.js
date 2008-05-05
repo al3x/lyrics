ba7b99526dddcf5bf31d64372a0a0c47560c19b8
Ed Finkler <coj@SniperWolf.local>
2008-05-05 00:43:52 -0400

    added form styling and help tooltips

---
+++ javascripts/app.js
@@ -0,0 +1,28 @@
+if (!LWL) { var LWL = {} };
+
+/*
+	Text used in the help tooltips for the form
+*/
+LWL.help = {
+	body	:"The lyrics you would like to share",
+	artist	:"The artist who wrote these lyrics",
+	song	:"The song in which these lyrics appear",
+	album	:"<strong>Optional.</strong> The album where the song appears",
+	ASIN	:"<strong>Optional.</strong> The Amazon ASIN code for the album or song",
+}
+
+
+LWL.initFormHelp = function() {
+	
+	for (fieldname in LWL.help) {
+		$("#"+fieldname+"-field").attr('title', LWL.help[fieldname]);
+		$("#"+fieldname+"-field").inputHintBox({
+												className:'tooltip',
+												source:'attr',
+												attr:'title',
+												incrementLeft:230,
+												incrementTop:40
+												});
+	}
+	
+}
